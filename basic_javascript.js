780ac092d7f4e3661bc0d54aa55bff34eb43e428
whynaa <whynaagustin@gmail.com>
2024-08-07 11:09:49 +0700

    initial commit

---
+++ basic_javascript.js
@@ -0,0 +1,26 @@
+// persegi
+
+class persegi {
+    constructor(sisi){
+        this.sisi = sisi
+    }
+
+    luas = () => {
+        return this.sisi * this.sisi
+    }
+
+    keliling = () => {
+        return 4 * this.sisi
+    }
+}
+
+let kotak = new persegi(5)
+console.log("Luas : " + kotak.luas())
+console.log("Keliling : " + kotak.keliling())
+
+// class kubus extends persegi {
+//     // kll 6 x sisi
+//     // l 6 x sisi^2
+//     // vol sisi^3
+// }
+
